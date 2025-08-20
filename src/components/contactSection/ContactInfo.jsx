4fa1e3824c132035bf2ae117abae5c2e449057ec
Sadelto0130 <49660985+Sadelto0130@users.noreply.github.com>
2025-08-20 14:00:11 -0500

    envio de mails contact me arreglado

--- src/components/contactSection/ContactInfo.jsx
+++ src/components/contactSection/ContactInfo.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 import { IoLocation } from "react-icons/io5";
 import SingleInfo from "./SingleInfo";
 
-const numeroTelefono = "573024781543";
+const numeroTelefono = "541136484027";
 const texto = `Hola, estoy interesado en tus servicios. Me gustaría saber más sobre lo que ofreces`;
 const url = `https://wa.me/${numeroTelefono}?text=${encodeURIComponent(texto)}`;
 
@@ -17,7 +17,7 @@ const ContactInfo = () => {
         className="inline-flex items-center gap-2 px-4 py-2 rounded-lg text-white bg-[#25d3658e] hover:bg-[#1ebe5b] transition-all duration-300"
       >
         <FaWhatsapp className="text-xl" />
-        <span className="text-base font-medium">+57 302 478 1543</span>
+        <span className="text-base font-medium">Contact Me</span>
       </a>
       <SingleInfo text="sadelto3011@gmail.com" Image={HiOutlineMail} />
       <SingleInfo text="Cartagena, Colombia" Image={IoLocation} />

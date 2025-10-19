38e6c9747962bf34d9f379a54ebc132334813819
victor salazar <49660985+Sadelto0130@users.noreply.github.com>
2025-10-19 14:38:27 -0300

    Actualizar ExperienceTopLeft.jsx

--- src/components/experienceSection/ExperienceTopLeft.jsx
+++ src/components/experienceSection/ExperienceTopLeft.jsx
@@ -8,7 +8,7 @@ const  ExperienceTopLeft = () => {
       <div className='flex justify-center items-center gap-4'>
         <ExperienceInfo number='1' text='Year' />
         <p className='font-bold text-6xl text-lightGrey'>-</p>
-        <ExperienceInfo number='2' text='Projects' />
+        <ExperienceInfo number='3' text='Projects' />
       </div>
       <p className='text-center text-white'>
         Fullstack developer with 1 year of experience, 3 web projects, clean code, and fast learning mindset.
